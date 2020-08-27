220ae34ec1a37c95a662e3d6e04e6d104199d38c
Crowdbotics <team@crowdbotics.com>
2020-08-27 21:55:08 +0000

    Update models

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ratio_19830.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ratio_19830.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
